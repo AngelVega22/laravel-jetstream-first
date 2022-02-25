ac6069cb1ea6f617242e8ba4737fa9c7ec0745d8
Angel <avegasam@gmail.com>
2022-02-25 00:21:39 -0500

    hello world

--- routes/web.php
+++ routes/web.php
@@ -17,35 +17,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/hola', function(){
-//     return '<h1> hola mundo<h1>';
-// });
+Route::get('/hola', function(){
+    return '<h1> hola mundo<h1>';
+});
 
-// Route::get('/prueba', function(){
-//     $array = ['js','php','c'];
-//     return $array;
-// });
+Route::get('/prueba', function(){
+    $array = ['js','php','c'];
+    return $array;
+});
 
-// Route::get('/nombre/{nombre}',function($nombre){
-//     return '<h1>Mi nombre es: ' .$nombre. '</h1>';
-// });
+Route::get('/nombre/{nombre}',function($nombre){
+    return '<h1>Mi nombre es: ' .$nombre. '</h1>';
+});
 
-// Route::get('/cliente/{cliente?}',function($cliente ='Cliente random'){
-//     return '<h1>El Cliente es: ' .$cliente. '</h1>';
-// });
+Route::get('/cliente/{cliente?}',function($cliente ='Cliente random'){
+    return '<h1>El Cliente es: ' .$cliente. '</h1>';
+});
 
-// Route::get('/ruta1', function(){
-//     return '<h1>Esta es la ruta 1<h1>';
-// }) ->name('ruta1');
+Route::get('/ruta1', function(){
+    return '<h1>Esta es la ruta 1<h1>';
+}) ->name('ruta1');
 
-// Route::get('/ruta2', function(){
-//     // return '<h1>Esta es la ruta 2<h1>';
-//     return redirect() ->Route('ruta1');
-// }) ;
+Route::get('/ruta2', function(){
+    // return '<h1>Esta es la ruta 2<h1>';
+    return redirect() ->Route('ruta1');
+}) ;
 
-// Route::get('/usuario/{usuario}', function($usuario){
-//     return 'El usuario es:' .$usuario;
-// })-> where('usuario','[0-9]+');
+Route::get('/usuario/{usuario}', function($usuario){
+    return 'El usuario es:' .$usuario;
+})-> where('usuario','[0-9]+');
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
