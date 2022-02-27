cbdaf82eb680a0e0e5293f2217076c2028b300d5
Angel <avegasam@gmail.com>
2022-02-26 22:21:09 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('/datos','App\Http\Controllers\CategoriaController');
+
 Route::get('/hola', 'App\Http\Controllers\InicioController@index' );
 
 
